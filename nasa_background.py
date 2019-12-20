9570dfa0088c984b56b3ae9a7f9c768681acfd4e
Thomas Wesselink <wesselink.tc@gmail.com>
2019-12-20 16:48:27 +0100

    Basic download functionality

--- nasa_background.py
+++ nasa_background.py
@@ -1,18 +1,26 @@
 import click
 
+from utils import nasa_api
+
+
 @click.group()
 def nasa_background():
     pass
 
 @nasa_background.command()
-def cmd1():
-    '''Command on nasa_background'''
-    click.echo('nasa_background cmd1')
+def update():
+    '''Get the newest NASA Picture of the Day and set it as background'''
+    try:
+        meta_info = nasa_api.get_info()
+        click.echo(f"Title: {meta_info['title']}\n")
+        click.echo(meta_info['explanation'] + "\n")
 
-@nasa_background.command()
-def cmd2():
-    '''Command on nasa_background'''
-    click.echo('nasa_background cmd2')
+        if click.confirm("Do you wish to download this image?"):
+            nasa_api.download_image()
+
+    except Exception as e:
+        click.echo("Fatal error encountered, exiting program..", err=True)
+
 
 if __name__ == '__main__':
     nasa_background()
---
+++ utils/nasa_api.py
@@ -0,0 +1,66 @@
+import os
+from datetime import datetime
+
+import click
+import requests
+
+IMAGE_FOLDER = os.path.join(os.path.expanduser('~/Pictures'), 'NASA')
+URL = 'https://api.nasa.gov/'
+POD_ENDPOINT = 'planetary/apod'
+API_KEY = 'DEMO_KEY'
+
+if not os.path.exists(IMAGE_FOLDER):
+    os.makedirs(IMAGE_FOLDER)
+
+def get_info(date=datetime.today()):
+    """
+    Downloads the meta-info about picture of the day for specified date
+
+    Arguments:
+    date -- date for which POD should be retrieved, defaults to today
+
+    Output:
+    Dictionary with meta-information about POD
+    """
+    try:
+        params = {
+            'date': date.strftime("%Y-%m-%d"),
+            'hd': True,
+            'api_key': API_KEY
+        }
+
+        response = requests.get(URL + POD_ENDPOINT, params)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        click.echo(f"Could not download meta-info for POD {date.date()}..")
+        raise e
+
+
+def download_image(date=datetime.today()):
+    """
+    Downloads the hd image for a specified date, and shows progress bar
+
+    Arguments:
+    date -- date for which POD should be retrieved, defaults to today
+    """
+    try:
+        meta_info = get_info(date=date)
+        url = meta_info['hdurl']
+
+        title = meta_info['title'].replace(' ', '_')
+        img_name = f"{date.strftime('%Y-%m-%d')}_{title}.jpg"
+        img_path = os.path.join(IMAGE_FOLDER, img_name)
+
+        response = requests.get(url, stream=True)
+        total_size = int(response.headers.get('content-length'))
+
+        with open(img_path, 'wb') as local_file:
+            with click.progressbar(
+                    length=total_size, label=f"Downloading - {meta_info['title']} ({date.date()})") as bar:
+                for data in response.iter_content(chunk_size=4096):
+                    local_file.write(data)
+                    bar.update(len(data))
+    except Exception as e:
+        click.echo("Could not download")
+        raise e
