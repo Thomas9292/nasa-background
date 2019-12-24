fb99088ee77a0effb3863f506170f21b256bca58
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-24 13:37:40 -0800

    Add support for YYYY-M-D strings.

--- tools/utils.py
+++ tools/utils.py
@@ -1,6 +1,4 @@
 def parse_str_to_date(date):
-    """ Given a date which is a string, will attempt to parse it by first splitting the string on its non numeric characters,
-    and then by partitioning YYYYMMDD from the string. """
     import re
     from datetime import datetime
     date_str_arr = list(filter(lambda x: x.isdigit(), re.split(r"(\d+)", date)))
@@ -13,6 +11,11 @@ def parse_str_to_date(date):
             date_str_arr.append(date_str[:4])
             date_str_arr.append(date_str[4:6])
             date_str_arr.append(date_str[6:])
+
+    strs = [None] * 3
+    strs[0] = date_str_arr[0].zfill(4)
+    strs[1] = date_str_arr[1].zfill(2)
+    strs[2] = date_str_arr[2].zfill(2)
 
     date = datetime.strptime("-".join(date_str_arr[:3]), "%Y-%m-%d")
     return date
