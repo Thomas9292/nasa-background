42545959383298323c9d90d8f43f156d061bcb5a
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-24 13:36:13 -0800

    Add support for a --date command.
    It is now possible to pass a date string in YYYYMMDD format to choose a given date, though if none is passed, defaults to the current date.

--- nasa_background.py
+++ nasa_background.py
@@ -1,22 +1,32 @@
 import click
 
 from tools import nasa_api, background
+from tools.utils import parse_str_to_date
 
 
 @click.group()
-def nasa_background():
-    pass
+@click.option("--date", default=None, help="Enter the date as a single string in YYYY-MM-DD format.")
+@click.pass_context
+def nasa_background(ctx, date):
+    from datetime import datetime
+    if date is None:
+        date = datetime.now()
+    else:
+        date = parse_str_to_date(date)
+    ctx.obj = {"DATE": date}
+
 
 @nasa_background.command()
-def update():
+@click.pass_context
+def update(ctx):
     '''Get the newest NASA Picture of the Day and set it as background'''
     try:
-        meta_info = nasa_api.get_info()
+        meta_info = nasa_api.get_info(ctx.obj["DATE"])
         click.echo(f"Title: {meta_info['title']}\n")
         click.echo(meta_info['explanation'] + "\n")
 
         if click.confirm("Do you wish to download this image and set it as background?"):
-            file_path = nasa_api.download_image()
+            file_path = nasa_api.download_image(ctx.obj["DATE"])
             background.change_background(file_path)
 
     except Exception as e:
