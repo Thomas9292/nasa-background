1362249d2a7d05f007d6cf0dd5c3235e3d16683b
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-24 13:48:51 -0800

    Simplify parse_str_to_date, add doc string.

--- tools/utils.py
+++ tools/utils.py
@@ -1,8 +1,12 @@
 def parse_str_to_date(date):
+    """ Given some date as a string, will attempt to parse it to a date, if it can be split into
+    3 or more sections of numeric characters seperated by non-numeric characters. Otherwise, it will
+    check if there are exactly eight digits: if so, it will split the string into YYYYMMDD compontents.
+    Otherwise, it will fail. """
     import re
     from datetime import datetime
     date_str_arr = list(filter(lambda x: x.isdigit(), re.split(r"(\d+)", date)))
-    if len(date_str_arr) != 3:
+    if len(date_str_arr) < 3:
         date_str = "".join(date_str_arr)
         if len(date_str) != 8:
             raise ValueError("Insufficient digits to reconstruct a YYYY-mm-dd date")
@@ -11,11 +15,6 @@ def parse_str_to_date(date):
             date_str_arr.append(date_str[:4])
             date_str_arr.append(date_str[4:6])
             date_str_arr.append(date_str[6:])
-
-    strs = [None] * 3
-    strs[0] = date_str_arr[0].zfill(4)
-    strs[1] = date_str_arr[1].zfill(2)
-    strs[2] = date_str_arr[2].zfill(2)
 
     date = datetime.strptime("-".join(date_str_arr[:3]), "%Y-%m-%d")
     return date
