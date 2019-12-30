28dd3fb0b851b4a79eb347006b256a82f24d2bfd
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-30 11:26:49 -0800

    Move date option to update command.

--- nasa_background.py
+++ nasa_background.py
@@ -5,32 +5,31 @@ from tools.utils import parse_str_to_date
 
 
 @click.group()
+def nasa_background():
+    pass
+
+
+@nasa_background.command()
 @click.option("--date", default=None, help="Enter the date as a single string in YYYYMMDD or YYYY-MM-DD format.")
-@click.pass_context
-def nasa_background(ctx, date):
+def update(date):
     from datetime import datetime
+
     if date is None:
         date = datetime.now()
     else:
         date = parse_str_to_date(date)
     print(date)
-    ctx.obj = {"DATE": date}
-
-
-@nasa_background.command()
-@click.pass_context
-def update(ctx):
     '''Get the newest NASA Picture of the Day and set it as background'''
     try:
-        meta_info = nasa_api.get_info(ctx.obj["DATE"])
+        meta_info = nasa_api.get_info(date)
         click.echo(f"Title: {meta_info['title']}\n")
         click.echo(meta_info['explanation'] + "\n")
 
         if click.confirm("Do you wish to download this image and set it as background?"):
-            file_path = nasa_api.download_image(ctx.obj["DATE"])
+            file_path = nasa_api.download_image(date)
             background.change_background(file_path)
     except KeyError:
-        click.echo(f"Image not found for the selected date {ctx.obj['DATE']}. ")
+        click.echo(f"Image not found for the selected date {date}. ")
     except Exception as e:
         click.echo("Fatal error encountered, exiting program.")
         click.echo(e)
