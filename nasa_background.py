249f5d6cf49ea880f1b70873d1f57c7e244acb5a
Thomas Wesselink <wesselink.tc@gmail.com>
2020-01-07 14:35:50 +0100

    Refactor and comment code

--- nasa_background.py
+++ nasa_background.py
@@ -12,27 +12,27 @@ def nasa_background():
 @nasa_background.command()
 @click.option("--date",
               default=None,
-              help="Enter the date as a single string in YYYYMMDD or YYYY-MM-DD format."
-              )
+              help="Enter the date as a single string in YYYYMMDD or YYYY-MM-DD format." )
 @click.option("--auto",
               is_flag=True,
-              help="Disables prompts and sets the background automatically if this can succefully be completed.s"
-              )
+              help="Disables prompts and sets the background automatically if this can succefully be completed.s" )
 def update(date, auto):
-    from datetime import datetime
-
+    '''Get the newest NASA Picture of the Day and set it as background'''
+    # Check if date is passed as argument, set to default (today) otherwise
     if date is None:
         date = datetime.now()
     else:
         date = parse_str_to_date(date)
 
-    '''Get the newest NASA Picture of the Day and set it as background'''
     try:
+        # Download and print information about
         meta_info = nasa_api.get_info(date)
         click.echo(f"Title: {meta_info['title']}\n")
         click.echo(meta_info['explanation'] + "\n")
 
+        # Check if auto is selected, otherwise prompt user to set it as background
         if auto or click.confirm("Do you wish to download this image and set it as background?"):
+            # Download and set the background
             file_path = nasa_api.download_image(date)
             background.change_background(file_path, auto)
     except KeyError:
